849baf6e9fb23bd6c81853638fa724f4c77a7abe
Menuka Gusain <menukagusain@gmail.com>
2021-03-15 12:28:46 -0400

    udated files with new changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,10 +1,10 @@
 const path = require("path");
 // For HTML Routing
 module.exports = (app) => {
- 
-    //  HTML GET Requests
-//   Below code handles when users "visit" a page.
-
+//   // => HTML GET Requests
+//   // Below code handles when users "visit" a page.
+//   // In each of the below cases the user is shown an HTML page of content
+// return from notes.html
     app.get("/notes", function (_req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
@@ -13,3 +13,4 @@ module.exports = (app) => {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 };
+
--- server.js
+++ server.js
@@ -1,39 +1,33 @@
 // DEPENDENCIES
-// Npm packages that we will use to give our server useful functionality
-
+// Series of npm packages that we will use to give our server useful functionality
 const fs = require("fs");
 const express = require('express');
 const path = require("path");
 
-
-// Intialize express app
-
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 const app = express();
-
 // Sets an initial port. We"ll use this later in our listener
-
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static('./develop/public'));
-
-//Require routes file
-
-require("./develop/routes/apiRoutes")(app);
-require("./develop/routes/htmlRoutes")(app);
+app.use(express.static(__dirname + "/public"));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
 
 // Tells node that we are creating an "express" server
-const dbJson = require("./develop/db/db.json");
+// const dbJson = require("/db/db.json");
 
 
-// Setup Listener 
-
+// LISTENER
+// The below code effectively "starts" our server
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
